e9d8e47ae986d22275bece473c470aec436482aa
EdwarUlianov <eduarmrm@gmail.com>
2015-05-23 23:52:45 +0000

    Añadidas nueva validaciones para el modelo Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,7 @@ class Usuario < ActiveRecord::Base
   validates :nombre, presence: true, uniqueness: true
   validates :nacimiento, presence: true
   validates :altura, numericality: { only_integer: true, greater_than: 0 }
+  validates :sexo, presence: true
+  validates :complexion, presence: true
+  
 end
