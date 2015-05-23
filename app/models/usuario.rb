9de0ce974b9c648458e9e88194db6a44d12278ea
camador <camador.git@gmail.com>
2015-05-23 20:43:58 +0200

    Añadidas validaciones al modelo Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,6 @@
 class Usuario < ActiveRecord::Base
+
+  validates :nombre, presence: true, uniqueness: true
+  validates :nacimiento, presence: true
+  validates :altura, numericality: { only_integer: true, greater_than: 0 }
 end
