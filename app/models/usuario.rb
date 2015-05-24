79bf415da9d7f9631d14abc58b1a0c8e1694122e
camador <camador.git@gmail.com>
2015-05-24 10:21:13 +0200

    El campo 'altura' no tiene por qué ser entero

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,8 +2,8 @@ class Usuario < ActiveRecord::Base
 
   validates :nombre, presence: true, uniqueness: true
   validates :nacimiento, presence: true
-  validates :altura, numericality: { only_integer: true, greater_than: 0 }
+  validates :altura, numericality: { greater_than: 0 }
   validates :sexo, presence: true
   validates :complexion, presence: true
-  
+
 end
