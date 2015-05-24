c62fbb5accc3fc0a0ccd34f038a9b004a219516d
camador <camador.git@gmail.com>
2015-05-24 10:28:33 +0200

    Mejoras para el campo 'sexo': validación y descripción de valores

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,9 +1,23 @@
 class Usuario < ActiveRecord::Base
 
+  # Descripciones de los valores del campo 'sexo'
+  SEXO = {'Hombre' => 'H',
+          'Mujer' => 'M'}
+
   validates :nombre, presence: true, uniqueness: true
   validates :nacimiento, presence: true
   validates :altura, numericality: { greater_than: 0 }
-  validates :sexo, presence: true
+  validates :sexo, inclusion: { in: %w(H M) }
   validates :complexion, presence: true
 
+  # Descripción del valor del campo 'sexo'
+  def sexo_descripcion
+    SEXO.index(self.sexo)
+  end
+
+  # Fija el valor del campo 'sexo' en función de la descripción
+  # recibida
+  def sexo_descripcion=(descripcion)
+    self.sexo = SEXO[descripcion]
+  end
 end
