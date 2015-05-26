e504f5f2c01fc062314969319b6f83d9a9418924
EdwarUlianov <eduarmrm@gmail.com>
2015-05-26 17:10:17 +0000

    Añadido formulario de nuevo usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,7 +1,26 @@
 class UsuariosController < ApplicationController
     
-    def new
+  def new
+    @usuario = Usuario.new
+  end
+  
+  def show
+    @usuario = Usuario.find(params[:id])
+  end
+    
+  def create
+    @usuario = Usuario.create(usuario_params)
+    if @usuario.save 
+      redirect_to "/usuarios/#{@usuario.id}", :notice => "Usuario Creado!"
+    else
+      render "new"
     end
+  end
+    
+  private
+    
+  def usuario_params
+    params.require(:usuario).permit(:nombre, :altura, :nacimiento, :sexo, :complexion)
+  end
 
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   
   resources :usuarios
   
+  
+ 
+  
 end
