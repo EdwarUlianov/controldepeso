52371a22df606330e3256e0488eaa3fe670861bd
camador <camador.git@gmail.com>
2015-05-31 21:50:01 +0200

    Añadidos tests para nacimiento, altura, sexo y complexion

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -6,11 +6,13 @@ class UsuarioTest < ActiveSupport::TestCase
     @usuario = usuarios(:valido)
   end
 
+  # Válido
   test 'usuario debería ser válido' do
     assert  @usuario.valid?
   end
 
-  test 'nombre no puede ser blanco' do
+  # Nombre
+  test 'nombre debe estar presente' do
     @usuario.nombre = ' '
     assert_not @usuario.valid?
   end
@@ -18,6 +20,80 @@ class UsuarioTest < ActiveSupport::TestCase
   test 'nombre debe ser único' do
     duplicado = @usuario.dup
     assert_not duplicado.valid?
+  end
+
+  # Nacimiento
+  test 'nacimiento debe estar presente' do
+    @usuario.nacimiento = nil
+    assert_not @usuario.valid?
+  end
+
+  # Altura
+  test 'altura debe estar presente' do
+    @usuario.altura = nil
+    assert_not @usuario.valid?
+  end
+
+  test 'altura debe ser un número' do
+    @usuario.altura = 'a'
+    assert_not @usuario.valid?
+  end
+
+  test 'altura debe ser mayor que cero' do
+    @usuario.altura = 0
+    assert_not @usuario.valid?
+  end
+
+  test 'altura no puede ser negativo' do
+    @usuario.altura = -1
+    assert_not @usuario.valid?
+  end
+
+  # Sexo
+  test '"H" es un valor válido para sexo' do
+    @usuario.sexo = 'H'
+    assert @usuario.valid?
+  end
+
+  test '"M" es un valor válido para sexo' do
+    @usuario.sexo = 'M'
+    assert @usuario.valid?
+  end
+
+  test 'sexo debe estar presente' do
+    @usuario.sexo = nil
+    assert_not @usuario.valid?
+  end
+
+  test 'sexo debe tener un valor válido' do
+    @usuario.sexo = 'v'
+    assert_not @usuario.valid?
+  end
+
+  # Complexión
+  test '"D" es un valor válido para sexo' do
+    @usuario.complexion = 'D'
+    assert @usuario.valid?
+  end
+
+  test '"N" es un valor válido para sexo' do
+    @usuario.complexion = 'N'
+    assert @usuario.valid?
+  end
+
+  test '"F" es un valor válido para sexo' do
+    @usuario.complexion = 'F'
+    assert @usuario.valid?
+  end
+
+  test 'complexion debe estar presente' do
+    @usuario.complexion = nil
+    assert_not @usuario.valid?
+  end
+
+  test 'complexion debe tener un valor válido' do
+    @usuario.complexion = 'v'
+    assert_not @usuario.valid?
   end
 
 end
