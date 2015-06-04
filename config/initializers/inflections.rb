086ea94ded04ff9450b471f71c9e6a21d3f50e05
camador <camador.git@gmail.com>
2015-06-04 10:56:24 +0200

    Añadidas inflexiones para locale :es (y :en para el generate)

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,36 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.plural(/^(.*)tud$/i, '\1tudes')       # Solicitud - Solicitudes
+  inflect.singular(/^(.*)tudes$/i, '\1tud')
+  inflect.plural(/^(.*)ad$/i, '\1ades')         # Ciudad - Ciudades
+  inflect.singular(/^(.*)dades$/i, '\1dad')
+  inflect.plural(/^(.*)on$/i, '\1ones')         # Profesión - Profesiones
+  inflect.singular(/^(.*)ones$/i, '\1on')
+  inflect.plural(/^(.*)or$/i, '\1ores')         # Profesor - Profesores
+  inflect.singular(/^(.*)ores$/i, '\1or')
+  inflect.plural(/^(.*)ar$/i, '\1ares')         # Lugar - Lugares
+  inflect.singular(/^(.*)ares$/i, '\1ar')
+  inflect.plural(/^(.*[aeiou])l$/i, '\1les')    # Plural - Plurales
+  inflect.singular(/^(.*[aeiou])les$/i, '\1l')
+end
+
+ActiveSupport::Inflector.inflections(:es) do |inflect|
+  inflect.plural(/^(.*)tud$/i, '\1tudes')       # Solicitud - Solicitudes
+  inflect.singular(/^(.*)tudes$/i, '\1tud')
+  inflect.plural(/^(.*)ad$/i, '\1ades')         # Ciudad - Ciudades
+  inflect.singular(/^(.*)dades$/i, '\1dad')
+  inflect.plural(/^(.*)on$/i, '\1ones')         # Profesión - Profesiones
+  inflect.singular(/^(.*)ones$/i, '\1on')
+  inflect.plural(/^(.*)or$/i, '\1ores')         # Profesor - Profesores
+  inflect.singular(/^(.*)ores$/i, '\1or')
+  inflect.plural(/^(.*)ar$/i, '\1ares')         # Lugar - Lugares
+  inflect.singular(/^(.*)ares$/i, '\1ar')
+  inflect.plural(/^(.*[aeiou])l$/i, '\1les')    # Plural - Plurales
+  inflect.singular(/^(.*[aeiou])les$/i, '\1l')
+end
+
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
