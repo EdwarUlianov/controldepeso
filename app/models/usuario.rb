12e1401ac463ab5d377a3c843a7ea5b0a8d8f484
EdwarUlianov <eduarmrm@gmail.com>
2015-06-21 00:20:59 +0200

    Añadidos más tests al modelo Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -7,6 +7,7 @@ class Usuario < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   
   has_secure_password
+  
   validates :password, presence: true, length: { minimum: 6 }
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,16 +23,6 @@ ActiveRecord::Schema.define(version: 20150605174146) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "perfils", force: :cascade do |t|
-    t.string   "nombre"
-    t.date     "nacimiento"
-    t.float    "altura"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "sexo"
-    t.string   "complexion"
-  end
-
   create_table "usuarios", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
